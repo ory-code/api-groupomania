6c27c0c9475150515f1407d680471a3bce481db5
ory-code <orysacha@yahoo.be>
2021-07-24 13:42:54 +0200

    login function

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt")
-//const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const CryptoJS = require("crypto-js");
 
 exports.signup = (req, res, next) => {
@@ -24,6 +24,43 @@ exports.signup = (req, res, next) => {
         user
           .save()
           .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+          .catch((error) => res.status(500).json({ error }));
+      })
+      .catch((error) => res.status(500).json({ error }));
+  };
+
+
+  exports.login = (req, res, next) => {
+    if (!req.body.password || !req.body.email) {
+      return res.status(400).json({ error: "bad request" });
+    }
+    const email = CryptoJS.AES.encrypt(
+      req.body.email,
+      CryptoJS.enc.Utf8.parse(process.env.CRYPTO_SECRET_TOKEN),
+      {
+        mode: CryptoJS.mode.ECB,
+        iv: CryptoJS.enc.Utf8.parse(process.env.CRYPTO_IV),
+      }
+    ).toString();
+    User.findOne({ email: email })
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({ error: "identifiants incorrect !" });
+        }
+  
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              return res.status(404).json({ error: "identifiants incorrect !" });
+            }
+            res.status(200).json({
+              userId: user._id,
+              token: jwt.sign({ userId: user._id }, process.env.JWT_TOKEN, {
+                expiresIn: "1h",
+              }),
+            });
+          })
           .catch((error) => res.status(500).json({ error }));
       })
       .catch((error) => res.status(500).json({ error }));
