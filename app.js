6d1dbe34e1795f0d00478528553ddfa11c5ba9db
ory-code <orysacha@yahoo.be>
2021-07-24 14:03:12 +0200

    cors + init Sequelize

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const userRoutes = require("./routes/user");
 const swaggerJsdoc = require("swagger-jsdoc");
 const ui = require("swagger-ui-express");
-const path = require ("path")
-
+const path = require("path");
+const {Sequelize} = require("sequelize")
 const options = {
   definition: {
     openapi: "3.0.0",
@@ -13,13 +13,25 @@ const options = {
       version: "1.0.0",
     },
   },
-  apis: [path.join(__dirname,"./swagger.yaml")], // files containing annotations as above
+  apis: [path.join(__dirname, "./swagger.yaml")], // files containing annotations as above
 };
 const openapiSpecification = swaggerJsdoc(options);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.use("/api-docs", ui.serve);
 app.get("/api-docs", ui.setup(openapiSpecification));
-app.use("/api/user", userRoutes)
+app.use("/api/user", userRoutes);
 // app.get("/api-docs.json", (req, res) => {
 //   res.setHeader("Content-Type", "application/json");
 //   res.send(openapiSpecification);
