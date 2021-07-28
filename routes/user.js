f27cf24282bb3667658a81ac2097e3c038edc18b
ory-code <orysacha@yahoo.be>
2021-07-28 18:51:37 +0200

    routes ctrl

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const userCtrl = require("../controllers/user")
+const userCtrl = require("../controllers/user");
+const auth = require("../middleware/auth");
 
-router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login)
+router.post("/signup", auth, userCtrl.signup);
+router.post("/login", auth, userCtrl.login);
 
 module.exports = router;
