dfc74371e21db9a9db2fa40d9b2cfc81fef975c0
ory-code <orysacha@yahoo.be>
2021-07-28 18:51:29 +0200

    modif user signup

--- controllers/user.js
+++ controllers/user.js
@@ -1,70 +1,73 @@
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const CryptoJS = require("crypto-js");
 
 exports.signup = (req, res, next) => {
   console.log(req.body);
-    if (!req.body.password || !req.body.email) {
-      return res.status(400).json({ error: "bad request" });
+  if (!req.body.password || !req.body.email) {
+    return res.status(400).json({ error: "bad request" });
+  }
+  const name = req.body.name;
+  const firstname = req.body.firstname;
+  const sexe = req.body.sexe;
+  const email = CryptoJS.AES.encrypt(
+    req.body.email,
+    CryptoJS.enc.Utf8.parse(process.env.CRYPTO_SECRET_TOKEN),
+    {
+      mode: CryptoJS.mode.ECB,
+      iv: CryptoJS.enc.Utf8.parse(process.env.CRYPTO_IV),
     }
-    const name = req.body.name
-    const firstname = req.body.firstname
-    const email = CryptoJS.AES.encrypt(
-      req.body.email,
-      CryptoJS.enc.Utf8.parse(process.env.CRYPTO_SECRET_TOKEN),
-      {
-        mode: CryptoJS.mode.ECB,
-        iv: CryptoJS.enc.Utf8.parse(process.env.CRYPTO_IV),
-      }
-    ).toString();
-    bcrypt
-      .hash(req.body.password, 10)
-      .then((hash) => {
-        const user = new User({
-          email: email,
-          password: hash,
-        });
-        user
-          .save()
-          .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-          .catch((error) => res.status(500).json({ error }));
-      })
-      .catch((error) => res.status(500).json({ error }));
-  };
+  ).toString();
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        firstname: firstname,
+        name: name,
+        sexe: sexe,
+        email: email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
 
-
-  exports.login = (req, res, next) => {
-    if (!req.body.password || !req.body.email) {
-      return res.status(400).json({ error: "bad request" });
+exports.login = (req, res, next) => {
+  if (!req.body.password || !req.body.email) {
+    return res.status(400).json({ error: "bad request" });
+  }
+  const email = CryptoJS.AES.encrypt(
+    req.body.email,
+    CryptoJS.enc.Utf8.parse(process.env.CRYPTO_SECRET_TOKEN),
+    {
+      mode: CryptoJS.mode.ECB,
+      iv: CryptoJS.enc.Utf8.parse(process.env.CRYPTO_IV),
     }
-    const email = CryptoJS.AES.encrypt(
-      req.body.email,
-      CryptoJS.enc.Utf8.parse(process.env.CRYPTO_SECRET_TOKEN),
-      {
-        mode: CryptoJS.mode.ECB,
-        iv: CryptoJS.enc.Utf8.parse(process.env.CRYPTO_IV),
+  ).toString();
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ error: "identifiants incorrect !" });
       }
-    ).toString();
-    User.findOne({ email: email })
-      .then((user) => {
-        if (!user) {
-          return res.status(404).json({ error: "identifiants incorrect !" });
-        }
-  
-        bcrypt
-          .compare(req.body.password, user.password)
-          .then((valid) => {
-            if (!valid) {
-              return res.status(404).json({ error: "identifiants incorrect !" });
-            }
-            res.status(200).json({
-              userId: user._id,
-              token: jwt.sign({ userId: user._id }, process.env.JWT_TOKEN, {
-                expiresIn: "1h",
-              }),
-            });
-          })
-          .catch((error) => res.status(500).json({ error }));
-      })
-      .catch((error) => res.status(500).json({ error }));
-  };
+
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(404).json({ error: "identifiants incorrect !" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, process.env.JWT_TOKEN, {
+              expiresIn: "1h",
+            }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
