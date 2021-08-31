f8798540937be9b3d9bb753689f658bfead39bf1
ory-code <orysacha@yahoo.be>
2021-08-31 20:13:29 +0200

    like post

--- controllers/likePost.js
+++ controllers/likePost.js
@@ -1,97 +1,42 @@
-const Post = require("../models/post");
 const PostLike = require("../models/postLike");
 
 exports.likePost = (req, res, next) => {
-  //Params
-  const LIKED = 1;
-  const postid = req.params.postid;
-  const userid = req.params.userid;
-
-  Post.findOne({
-    where: { id: postid }, //On recupère l'id du post
-  })
-    .then((post) => {
-      PostLike.findOne({
-        attributes: ["id", "userid", "Postid", "liketype"],
-        where: { Userid: userid, Postid: postid },
-      })
-        .then((like) => {
-          if (like) {
-            //Si oui
-            if (like.liketype === LIKED) {
-              return res.status(409).json({ message: "Déjà liké" }); //Erreur conflit
-            }
-            return like
-              .update({ liketype: LIKED }) //On update le like
-              .then(() => {
-                post
-                  .update({ like: post.like + 1 }) //On update le post
-                  .then(res.status(201).json({ message: "Liké !" })) //, post, like
-                  .catch(
-                    res.status(500).json({ error: " Erreur update post" })
-                  ); //Erreur server
-              })
-              .catch(res.status(500).json({ error: "error" })); //Erreur server
-          }
-          PostLike.create({ Userid: userid, Postid: postid, liketype: LIKED }) //Si non
-            .then(() => {
-              post
-                .update({ like: post.like + 1 }) //On update le post
-                .then(res.status(201).json({ message: "Liké !" }))
-                .catch(res.status(500).json({ error })); //Erreur server
-            })
-            .catch(res.status(500).json({ error: "error" })); //Erreur server
-        })
-        .catch(res.status(406).json({ error: "error" })); //Erreur not accetable
+  const postId = req.params.id;
+  const userId = res.locals.userId;
+  const liked = 1;
+  const likedPost = new PostLike({
+    postid: postId,
+    userid: userId,
+    liketype: liked,
+  });
+  console.log(liked);
+  likedPost
+    .save()
+    .then(() => {
+      res.status(200).json({ message: " Welldone post liked !" });
     })
-    .catch(res.status(406).json({ error: "error" })); //Erreur not accetable
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json(error);
+    });
 };
-
 exports.dislikePost = (req, res, next) => {
-  //Params
-  const DISLIKED = -1;
-  const postid = req.params.postid;
-  const userid = req.params.userid;
-
-  Post.findOne({
-    attributes: ["id", "liketype"],
-    where: { id: postid }, //On recupère l'id du post
-  })
-    .then((post) => {
-      PostLike.findOne({
-        attributes: ["id", "Userid", "Postid", "liketype"],
-        where: { Userid: userid, Postid: postid },
-      })
-        .then((like) => {
-          if (like) {
-            //Si oui
-            if (like.liketype === DISLIKED) {
-              return res.status(409).json({ message: "Déjà disliké" }); //Erreur conflit
-            }
-            return like
-              .update({ liketype: DISLIKED }) //On update le dislike
-              .then((like) => {
-                post
-                  .update({ like: post.like - 1 }) //On update le post
-                  .then(res.status(201).json({ message: "Disliké !!" })) //, post, like
-                  .catch(res.status(500).json({ error: "error" })); //Erreur server
-              })
-              .catch(res.status(500).json({ error: "error" })); //Erreur server
-          }
-          PostLike.create({
-            Userid: userid,
-            Postid: postid,
-            liketype: DISLIKED,
-          }) //Si non
-            .then((like) => {
-              post
-                .update({ like: post.like - 1 }) //On update le post
-                .then(res.status(201).json({ message: "Disliké !" }))
-                .catch(res.status(500).json({ error: "error" })); //Erreur server
-            })
-            .catch(res.status(500).json({ error: "error" })); //Erreur server
-        })
-        .catch(res.status(406).json({ error: "error" })); //Erreur not accetable
+  const postId = req.params.id;
+  const userId = res.locals.userId;
+  const disLiked = -1;
+  const likedPost = new PostLike({
+    postid: postId,
+    userid: userId,
+    liketype: disLiked,
+  });
+  console.log(disLiked);
+  likedPost
+    .save()
+    .then(() => {
+      res.status(200).json({ message: " Welldone post disliked !" });
     })
-    .catch(res.status(406).json({ error: "error" })); //Erreur not accetable
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json(error);
+    });
 };
--- routes/post.js
+++ routes/post.js
@@ -5,11 +5,11 @@ const postCtrl = require("../controllers/post");
 const likePostCtrl = require("../controllers/likePost");
 const auth = require("../middleware/auth");
 
-router.post("/", auth, multer, postCtrl.createPost); 
-router.post("/:id/like", auth, likePostCtrl.likePost); 
-router.get("/", auth, postCtrl.getAllPost); 
-router.get("/:id", auth, postCtrl.getOnePost); 
-router.put("/:id", auth, multer, postCtrl.updatePost); 
-router.delete("/:id", auth, postCtrl.deletePost); 
+router.post("/", auth, multer, postCtrl.createPost);
+router.post("/:id/like", auth, likePostCtrl.likePost, likePostCtrl.dislikePost);
+router.get("/", auth, postCtrl.getAllPost);
+router.get("/:id", auth, postCtrl.getOnePost);
+router.put("/:id", auth, multer, postCtrl.updatePost);
+router.delete("/:id", auth, postCtrl.deletePost);
 
 module.exports = router;
