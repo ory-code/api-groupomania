46c363888f5c668e2beba7bbd3b556187d7e61d5
ory-code <orysacha@yahoo.be>
2021-09-16 21:39:28 +0200

    cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
 const userRoutes = require("./routes/user");
 const postRoutes = require("./routes/post");
 const profilRoutes = require("./routes/profil");
@@ -35,18 +36,8 @@ const options = {
   apis: [path.join(__dirname, "./swagger.yaml")], // files containing annotations as above
 };
 const openapiSpecification = swaggerJsdoc(options);
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+
+app.use(cors())
 app.use(helmet())
 app.use(bodyParser.json());
 app.use("/api-docs", ui.serve);
