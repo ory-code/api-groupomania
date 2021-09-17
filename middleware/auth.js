89cd613cb4a3ee6db35b664110364de7090e43d3
ory-code <orysacha@yahoo.be>
2021-09-17 18:28:34 +0200

    update middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, process.env.JWT_TOKEN);
-    const userId = decodedToken.userId;
+    const userId = decodedToken;
     console.log(decodedToken);
     if (req.body.userId && req.body.userId !== userId) {
       throw "Invalid user ID";
