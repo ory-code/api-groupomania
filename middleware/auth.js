a64c1f4746ebc0561155145ec142c8a274afe3f4
ory-code <orysacha@yahoo.be>
2021-09-20 20:13:44 +0200

    correction token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
+  console.log(req.headers);
   try {
     const token = req.headers.authorization.split(" ")[1];
+    console.log(token);
     const decodedToken = jwt.verify(token, process.env.JWT_TOKEN);
-    const userId = decodedToken;
+
+    const userId = decodedToken.userId;
     console.log(decodedToken);
     if (req.body.userId && req.body.userId !== userId) {
       throw "Invalid user ID";
