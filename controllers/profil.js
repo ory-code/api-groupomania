4888fa5009a55914932fcb5d9e4b03b9ba376416
ory-code <orysacha@yahoo.be>
2021-10-21 12:28:07 +0200

    delete  profil function

--- controllers/profil.js
+++ controllers/profil.js
@@ -31,8 +31,8 @@ exports.updateProfil = (req, res, next) => {
 };
 
 exports.deleteProfil = (req, res, next) => {
-  const userId = res.locals.userId;
-  User.destroy({ where: { id: req.params.id, userid: userId } })
+  const id = req.params.id;
+  User.destroy({ where: { id: id } })
     .then(() => {
       res.status(200).json({ message: "profil delete !" });
     })
