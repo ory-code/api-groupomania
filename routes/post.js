86ddded74ad68c230fcdd838bff4f7c2797c3dc8
sacha <orysacha@yahoo.be>
2021-10-27 19:10:56 +0200

    delete like

--- routes/post.js
+++ routes/post.js
@@ -2,12 +2,9 @@ const express = require("express");
 const multer = require("../middleware/multer-config");
 const router = express.Router();
 const postCtrl = require("../controllers/post");
-const likePostCtrl = require("../controllers/likePost");
 const auth = require("../middleware/auth");
 
-router.post("/", auth, multer, postCtrl.createPost);
-router.post("/:id/like", auth, likePostCtrl.likePost);
-router.post("/:id/dislike", auth, likePostCtrl.dislikePost);
+router.post("/", auth, multer, postCtrl.createPost); 
 router.get("/", auth, postCtrl.getAllPost);
 router.get("/:id", auth, postCtrl.getOnePost);
 router.put("/:id", auth, multer, postCtrl.updatePost);
